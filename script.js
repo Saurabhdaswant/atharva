8f32f45a77d93d5ac89b4bbc13a4d3c4d6a112b1
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2021-05-12 10:23:58 +0530

    final commit 0.1
    changed the colors

--- script.js
+++ script.js
@@ -4,24 +4,24 @@ btn.addEventListener("click", function () {
   const randomColor = Math.trunc(Math.random() * 10) + 1;
   console.log(randomColor);
   if (randomColor === 1) {
-    document.body.style.background = "blue";
+    document.body.style.background = "#780000";
   } else if (randomColor === 2) {
-    document.body.style.background = "red";
+    document.body.style.background = "#ffff3f";
   } else if (randomColor === 3) {
-    document.body.style.background = "green";
+    document.body.style.background = "#283618";
   } else if (randomColor === 4) {
-    document.body.style.background = "yellow";
+    document.body.style.background = "#03045e";
   } else if (randomColor === 5) {
-    document.body.style.background = "pink";
+    document.body.style.background = "#52b788";
   } else if (randomColor === 6) {
-    document.body.style.background = "black";
+    document.body.style.background = "#affc41";
   } else if (randomColor === 7) {
-    document.body.style.background = "silver";
+    document.body.style.background = "#c77dff";
   } else if (randomColor === 8) {
-    document.body.style.background = "brown";
+    document.body.style.background = "#161a1d";
   } else if (randomColor === 9) {
-    document.body.style.background = "purple";
+    document.body.style.background = "#89023e";
   } else if (randomColor === 10) {
-    document.body.style.background = "orange";
+    document.body.style.background = "#ffba08";
   }
 });
