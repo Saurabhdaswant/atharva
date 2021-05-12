c72bf61b7bc9dfe07ed8e1b3cdabd1c6c7be849f
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2021-05-12 10:05:21 +0530

    generated the random number

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 const btn = document.getElementById("btn");
+
+btn.addEventListener("click", function () {
+  const randomColor = Math.trunc(Math.random() * 10) + 1;
+  console.log(randomColor);
+});
