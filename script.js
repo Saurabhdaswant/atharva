b1ec43703bf3377edbf62abe495aae5d425964af
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2021-05-13 10:48:45 +0530

    hurray final commit

--- script.js
+++ script.js
@@ -1,27 +1,15 @@
-const btn = document.getElementById("btn");
+let button = document.getElementById("btn");
+let colorHexName = document.getElementById("hex-value");
+let colorHex = [1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f "];
+let body = document.body.style;
 
-btn.addEventListener("click", function () {
-  const randomColor = Math.trunc(Math.random() * 10) + 1;
-  console.log(randomColor);
-  if (randomColor === 1) {
-    document.body.style.background = "#780000";
-  } else if (randomColor === 2) {
-    document.body.style.background = "#ffff3f";
-  } else if (randomColor === 3) {
-    document.body.style.background = "#283618";
-  } else if (randomColor === 4) {
-    document.body.style.background = "#03045e";
-  } else if (randomColor === 5) {
-    document.body.style.background = "#52b788";
-  } else if (randomColor === 6) {
-    document.body.style.background = "#affc41";
-  } else if (randomColor === 7) {
-    document.body.style.background = "#c77dff";
-  } else if (randomColor === 8) {
-    document.body.style.background = "#161a1d";
-  } else if (randomColor === 9) {
-    document.body.style.background = "#89023e";
-  } else if (randomColor === 10) {
-    document.body.style.background = "#ffba08";
+function fuck() {
+  let shit = "#";
+  for (let i = 0; i < 6; i++) {
+    shit += colorHex[Math.floor(Math.random() * colorHex.length)];
   }
-});
+
+  body.backgroundColor = shit;
+}
+
+button.addEventListener("click", fuck);
